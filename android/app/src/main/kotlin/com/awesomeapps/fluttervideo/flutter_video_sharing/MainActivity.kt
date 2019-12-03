7a7a4dcdb5239130ec458072080ee7b6397ac3e7
Jace Babin <jbabin91@gmail.com>
2019-12-03 15:48:45 -0700

    Start of video sharing app
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ android/app/src/main/kotlin/com/awesomeapps/fluttervideo/flutter_video_sharing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.awesomeapps.fluttervideo.flutter_video_sharing
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
